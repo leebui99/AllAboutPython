e83a5e53f0214fd44a2df9ede473cc23aedc2c43
Le Bui <leebui99@uw.edu>
2017-09-10 14:10:43 -0700

    Python Lab and Project

--- read_write_file_lebui.py
+++ read_write_file_lebui.py
@@ -1,16 +1,29 @@
 #CS-142 Lab 4
 #Read and Write story.txt
-#The number of characters
-#The number of letters
-#The number of lowercase letters
-#The number of consonants
+#The number of Lines
+#The number of Words
+#The number of Characters
 
 
 def main():
-    searchFile = open("story.txt", "r")
-    for line in searchFile:
-        print(line) #Print out the file contains
+    fname = "story.txt"
 
+    num_lines = 0
+    num_words = 0
+    num_chars = 0
+
+    with open(fname, 'r') as f:
+        for line in f:
+            words = line.split()
+
+            num_lines += 1
+            num_words += len(words)
+            num_chars += len(line)
+        
+    print("Lines ", num_lines) #Print out number of lines
+    print("Words ", num_words) #Print out number of words
+    print("Characters ", num_chars) #Print out number of characters
+    
 main()
 
 
